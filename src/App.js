8ee203554cd8f3dc39f675ce5e5d88adcf456075
navneshranjan <navneshranjan@gmail.com>
2022-03-15 13:34:59 +0530

    [Action,Reducer,Store all are working]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import "./App.css";
+import React from "react";
+import { incNumber, decNumber } from "./actions/index";
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <h1>ReduxStore</h1>
-    </div>
+    <>
+      <div className="container">
+        <h1>Increament/Decrement counter</h1>
+        <h3>Using react-redux</h3>
+        <div>
+          <a title="Decrement" onClick={() => dispatch(decNumber())}>
+            <span>-</span>
+          </a>
+          <input type="text" Value={myState} />
+          <a title="Increment" onClick={(state) => dispatch(incNumber())}>
+            <span>+</span>
+          </a>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
